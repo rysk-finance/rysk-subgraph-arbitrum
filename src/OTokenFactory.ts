e3c8d5d6b9c7beb5e87141113774029fa9519b53
Yassine <yassinesania@gmail.com>
2023-02-24 16:24:10 +0100

    refactor(exchange): Track all oTokens created
    we were only tracking the ones coming from Registry (which should be
    Exchage), should track all

--- src/OTokenFactory.ts
+++ src/OTokenFactory.ts
@@ -1,47 +1,42 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { AddressBook as AddressBookInterface } from '../generated/AddressBook/AddressBook'
-import { OtokenCreated } from "../generated/OTokenFactory/OTokenFactory"
-import { OToken as OTokenSource } from "../generated/templates"
-import { OToken as TokenContract } from "../generated/templates/OToken/OToken"
-import { OTokenFactory as FactoryInterface } from '../generated/OTokenFactory/OTokenFactory'
-import { OToken } from "../generated/schema"
-import { OPTION_REGISTRY } from "./helper"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { AddressBook as AddressBookInterface } from "../generated/AddressBook/AddressBook";
+import { OtokenCreated } from "../generated/OTokenFactory/OTokenFactory";
+import { OToken as OTokenSource } from "../generated/templates";
+import { OToken as TokenContract } from "../generated/templates/OToken/OToken";
+import { OTokenFactory as FactoryInterface } from "../generated/OTokenFactory/OTokenFactory";
+import { OToken } from "../generated/schema";
 
 export function handleOtokenCreated(event: OtokenCreated): void {
+  // Start indexing the newly created OToken contract
+  OTokenSource.create(event.params.tokenAddress);
 
-  const creator = event.params.creator
+  // bind to the address that emit the event
+  let factoryContract = FactoryInterface.bind(event.address);
+  let addressBookAddress = factoryContract.addressBook();
+  let addressBookContract = AddressBookInterface.bind(addressBookAddress);
+  let implementation = addressBookContract.getOtokenImpl();
 
-  if (creator.toHexString() == OPTION_REGISTRY.toLowerCase()) {
-    // Start indexing the newly created OToken contract
-    OTokenSource.create(event.params.tokenAddress)
+  // Create Otoken Entity
+  let entity = new OToken(event.params.tokenAddress.toHex());
 
-    // bind to the address that emit the event
-    let factoryContract = FactoryInterface.bind(event.address)
-    let addressBookAddress = factoryContract.addressBook()
-    let addressBookContract = AddressBookInterface.bind(addressBookAddress)
-    let implementation = addressBookContract.getOtokenImpl()
-    
-    // Create Otoken Entity
-    let entity = new OToken(event.params.tokenAddress.toHex())
+  entity.underlyingAsset = event.params.underlying.toHex();
+  entity.strikeAsset = event.params.strike.toHex();
+  entity.collateralAsset = event.params.collateral.toHex();
+  entity.strikePrice = event.params.strikePrice;
+  entity.isPut = event.params.isPut;
+  entity.expiryTimestamp = event.params.expiry;
+  entity.creator = event.params.creator;
+  entity.implementation = implementation;
 
-    entity.underlyingAsset = event.params.underlying.toHex()
-    entity.strikeAsset = event.params.strike.toHex()
-    entity.collateralAsset = event.params.collateral.toHex()
-    entity.strikePrice = event.params.strikePrice
-    entity.isPut = event.params.isPut
-    entity.expiryTimestamp = event.params.expiry
-    entity.creator = creator
-    entity.implementation = implementation
+  let contract = TokenContract.bind(event.params.tokenAddress);
+  // Access state variables and functions by calling them
+  entity.symbol = contract.symbol();
+  entity.name = contract.name();
+  entity.decimals = 8;
 
-    let contract = TokenContract.bind(event.params.tokenAddress)
-    // Access state variables and functions by calling them
-    entity.symbol = contract.symbol()
-    entity.name = contract.name()
-    entity.decimals = 8
+  entity.totalSupply = BigInt.fromI32(0);
+  entity.createdAt = event.block.timestamp;
+  entity.createdTx = event.transaction.hash;
 
-    entity.save()
-  }
-
-
+  entity.save();
 }
-
