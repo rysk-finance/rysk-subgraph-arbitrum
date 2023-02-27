0b8a59906b5f361f0a2a0a8e7518e6665c57fed2
Yassine <yassinesania@gmail.com>
2023-02-27 14:58:19 +0100

    feat(handlers): handleSeriesAltered
    Also update the entity IDs to a hash of expiry, strike and type so it
    can be used when doing the load on SeriesAltered

--- src/OptionCatalogue.ts
+++ src/OptionCatalogue.ts
@@ -1,32 +1,77 @@
-import { SeriesApproved } from "../generated/OptionCatalogue/OptionCatalogue"
-import { Serie, Expiry } from "../generated/schema"
+import { crypto, ByteArray, BigInt, log } from "@graphprotocol/graph-ts";
+import {
+  SeriesAltered,
+  SeriesApproved
+} from "../generated/OptionCatalogue/OptionCatalogue";
+import { Serie, Expiry } from "../generated/schema";
 
+const getEntityID = (
+  strike: BigInt,
+  expiration: BigInt,
+  isPut: boolean
+): string => {
+  return crypto
+    .keccak256(
+      ByteArray.fromUTF8(
+        strike
+          .toString()
+          .concat(expiration.toString())
+          .concat(isPut ? "P" : "C")
+      )
+    )
+    .toString();
+};
+
+// this creates a new Serie Entity and Expiry Entity if it doesn't exist
 export function handleSeriesApproved(event: SeriesApproved): void {
-    // Create Serie Entity
-    let entitySerie = new Serie(event.transaction.hash.toHex());
+  // TODO: it would be better if this just comes from the event
+  const entityID = getEntityID(
+    event.params.strike,
+    event.params.expiration,
+    event.params.isPut
+  );
 
-    entitySerie.strike = event.params.strike;
-    entitySerie.expiration = event.params.expiration;
-    entitySerie.isPut = event.params.isPut;
-    entitySerie.isBuyable = event.params.isBuyable;
-    entitySerie.isSellable = event.params.isSellable;
+  // Create Serie Entity
+  let entitySerie = new Serie(entityID);
 
-    entitySerie.save();
+  entitySerie.strike = event.params.strike;
+  entitySerie.expiration = event.params.expiration;
+  entitySerie.isPut = event.params.isPut;
+  entitySerie.isBuyable = event.params.isBuyable;
+  entitySerie.isSellable = event.params.isSellable;
 
-    // Create Expiry Entity if it doesn't exist
+  entitySerie.save();
 
-    let loadExpiry = Expiry.load(event.params.expiration.toString());
+  // Create Expiry Entity if it doesn't exist
 
-    if (loadExpiry == null) {
-        let entityExpiry = new Expiry(event.params.expiration.toString());
-        entityExpiry.timestamp = event.params.expiration;
-        entityExpiry.utc = "";
-        // entityExpiry.utc = new Date(event.params.expiration.toI32() * 1000).toUTCString();
-        entityExpiry.save();
+  let loadExpiry = Expiry.load(event.params.expiration.toString());
 
-
-    }
-
-
+  if (loadExpiry == null) {
+    let entityExpiry = new Expiry(event.params.expiration.toString());
+    entityExpiry.timestamp = event.params.expiration;
+    entityExpiry.utc = "";
+    // entityExpiry.utc = new Date(event.params.expiration.toI32() * 1000).toUTCString();
+    entityExpiry.save();
+  }
 }
 
+// this only updates isBuyable and isSellable
+export function handleSeriesAltered(event: SeriesAltered): void {
+  const entityID = getEntityID(
+    event.params.strike,
+    event.params.expiration,
+    event.params.isPut
+  );
+
+  let entitySerie = Serie.load(entityID);
+
+  if (entitySerie == null) {
+    log.error("Trying to update a Serie that doesn't exist", []);
+    return;
+  }
+
+  entitySerie.isBuyable = event.params.isBuyable;
+  entitySerie.isSellable = event.params.isSellable;
+
+  entitySerie.save();
+}
