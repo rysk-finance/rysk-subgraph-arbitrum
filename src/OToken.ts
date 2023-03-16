0aafdb558fce73a08ed4625f14773784e0d2eb78
ugolino <dan.ugolini@gmail.com>
2023-03-17 00:17:20 +0100

    fixed double counting positions

--- src/OToken.ts
+++ src/OToken.ts
@@ -1,48 +1,80 @@
 import { Transfer } from "../generated/templates/OToken/OToken"
 
-import { OToken, AccountBalance } from '../generated/schema'
+import { OToken, AccountBalance, OptionsTransferAction } from '../generated/schema'
 import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { BIGINT_ZERO, loadOrCreateAccount, loadOrCreatePosition, ZERO_ADDRESS } from "./helper"
+import { BIGINT_ZERO, LIQUIDITY_POOL, loadOrCreateAccount, loadOrCreatePosition, OPTION_REGISTRY, ZERO_ADDRESS } from "./helper"
 
 export function handleTransfer(event: Transfer): void {
   // Load oToken entity
   let entity = OToken.load(event.address.toHex())
   let amount = event.params.value
+
+  const toAddress = event.params.to
+  const fromAddress =  event.params.from
+
+  let excludedAddresses = [LIQUIDITY_POOL, OPTION_REGISTRY]
   
   // convert to 1e18 cause LP handleWriteOptions is 1e18 
   const amountLP = amount.times( BigInt.fromString('10000000000') )
 
-  if (event.params.from.toHex() == ZERO_ADDRESS) {
+  if (fromAddress.toHex() == ZERO_ADDRESS) {
     // Mint Operation
 
     // update account balance
-    let accountBalance = getOrCreateAccountBalance(event.params.to, entity as OToken)
+    let accountBalance = getOrCreateAccountBalance(toAddress, entity as OToken)
     accountBalance.balance = accountBalance.balance.plus(amount)
     accountBalance.save()
-
-  } else if (event.params.to.toHex() == ZERO_ADDRESS) {
+  } else if (toAddress.toHex() == ZERO_ADDRESS) {
     // Burn event
-
-    let accountBalance = getOrCreateAccountBalance(event.params.from, entity as OToken)
+    let accountBalance = getOrCreateAccountBalance(fromAddress, entity as OToken)
     accountBalance.balance = accountBalance.balance.minus(amount)
     accountBalance.save()
   } else {
-    let sourceAccount = getOrCreateAccountBalance(event.params.from, entity as OToken)
+    let sourceAccount = getOrCreateAccountBalance(fromAddress, entity as OToken)
     sourceAccount.balance = sourceAccount.balance.minus(amount)
     sourceAccount.save()
 
-    let destinationAccount = getOrCreateAccountBalance(event.params.to, entity as OToken)
+    let destinationAccount = getOrCreateAccountBalance(toAddress, entity as OToken)
     destinationAccount.balance = destinationAccount.balance.plus(amount)
     destinationAccount.save()
 
+    // exclude txs with options registry and liquidity pool since already handled by OptionsSold and Options Bought
+    if ( !(excludedAddresses.includes(toAddress.toHex()) || excludedAddresses.includes(fromAddress.toHex())) ) { 
+      let sourceAccountPosition = loadOrCreatePosition(fromAddress, event.address.toHex());
+      sourceAccountPosition.netAmount =  sourceAccountPosition.netAmount.minus( amountLP )
+      sourceAccountPosition.longAmount =  sourceAccountPosition.longAmount.minus( amountLP )
+      
 
-    let sourceAccountPosition = loadOrCreatePosition(event.params.from, event.address.toHex());
-    sourceAccountPosition.amount =  sourceAccountPosition.amount.minus( amountLP )
-    sourceAccountPosition.save()
+      let destinationAccountPosition = loadOrCreatePosition(toAddress, event.address.toHex());
+      destinationAccountPosition.netAmount =  destinationAccountPosition.netAmount.plus( amountLP )
+      destinationAccountPosition.longAmount =  destinationAccountPosition.longAmount.plus( amountLP )
+      
 
-    let destinationAccountPosition = loadOrCreatePosition(event.params.to, event.address.toHex());
-    destinationAccountPosition.amount =  destinationAccountPosition.amount.plus( amountLP )
-    destinationAccountPosition.save()
+      const optionsTransferTransaction = new OptionsTransferAction( event.transaction.hash.toHex() )
+
+      optionsTransferTransaction.otoken = event.address.toHex()
+      optionsTransferTransaction.from = fromAddress
+      optionsTransferTransaction.to = toAddress
+      optionsTransferTransaction.amount = amount
+      optionsTransferTransaction.timestamp = event.block.timestamp
+      optionsTransferTransaction.transactionHash = event.transaction.hash.toHex()
+
+      optionsTransferTransaction.save()
+
+      const sourceAccountOptionsTransferTransactions = sourceAccountPosition.optionsTransferTransactions
+      sourceAccountOptionsTransferTransactions.push( event.transaction.hash.toHex() )
+      sourceAccountPosition.optionsTransferTransactions = sourceAccountOptionsTransferTransactions   
+
+      const destinationAccountOptionsTransferTransactions = destinationAccountPosition.optionsTransferTransactions
+      destinationAccountOptionsTransferTransactions.push( event.transaction.hash.toHex() )
+      destinationAccountPosition.optionsTransferTransactions = destinationAccountOptionsTransferTransactions    
+
+
+      sourceAccountPosition.save()
+      destinationAccountPosition.save()
+
+    }
+
   }
 
 }
