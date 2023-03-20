c8949542650dad98a8fe3cd6afe0ac548540351e
Yassine <yassinesania@gmail.com>
2023-03-20 12:08:09 +0100

    style(prettier): TS files

--- src/OToken.ts
+++ src/OToken.ts
@@ -1,100 +1,143 @@
-import { Transfer } from "../generated/templates/OToken/OToken"
+import { Transfer } from "../generated/templates/OToken/OToken";
 
-import { OToken, AccountBalance, OptionsTransferAction } from '../generated/schema'
-import { Address, BigInt } from "@graphprotocol/graph-ts"
-import { BIGINT_ZERO, LIQUIDITY_POOL, loadOrCreateAccount, loadOrCreatePosition, OPTION_REGISTRY, ZERO_ADDRESS } from "./helper"
+import {
+  OToken,
+  AccountBalance,
+  OptionsTransferAction
+} from "../generated/schema";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import {
+  BIGINT_ZERO,
+  LIQUIDITY_POOL,
+  loadOrCreateAccount,
+  loadOrCreatePosition,
+  OPTION_REGISTRY,
+  ZERO_ADDRESS
+} from "./helper";
 
 export function handleTransfer(event: Transfer): void {
   // Load oToken entity
-  let entity = OToken.load(event.address.toHex())
-  let amount = event.params.value
+  let entity = OToken.load(event.address.toHex());
+  let amount = event.params.value;
 
-  const toAddress = event.params.to
-  const fromAddress =  event.params.from
+  const toAddress = event.params.to;
+  const fromAddress = event.params.from;
 
-  let excludedAddresses = [LIQUIDITY_POOL, OPTION_REGISTRY]
-  
-  // convert to 1e18 cause LP handleWriteOptions is 1e18 
-  const amountLP = amount.times( BigInt.fromString('10000000000') )
+  let excludedAddresses = [LIQUIDITY_POOL, OPTION_REGISTRY];
+
+  // convert to 1e18 cause LP handleWriteOptions is 1e18
+  const amountLP = amount.times(BigInt.fromString("10000000000"));
 
   if (fromAddress.toHex() == ZERO_ADDRESS) {
     // Mint Operation
 
     // update account balance
-    let accountBalance = getOrCreateAccountBalance(toAddress, entity as OToken)
-    accountBalance.balance = accountBalance.balance.plus(amount)
-    accountBalance.save()
+    let accountBalance = getOrCreateAccountBalance(toAddress, entity as OToken);
+    accountBalance.balance = accountBalance.balance.plus(amount);
+    accountBalance.save();
   } else if (toAddress.toHex() == ZERO_ADDRESS) {
     // Burn event
-    let accountBalance = getOrCreateAccountBalance(fromAddress, entity as OToken)
-    accountBalance.balance = accountBalance.balance.minus(amount)
-    accountBalance.save()
+    let accountBalance = getOrCreateAccountBalance(
+      fromAddress,
+      entity as OToken
+    );
+    accountBalance.balance = accountBalance.balance.minus(amount);
+    accountBalance.save();
   } else {
-    let sourceAccount = getOrCreateAccountBalance(fromAddress, entity as OToken)
-    sourceAccount.balance = sourceAccount.balance.minus(amount)
-    sourceAccount.save()
+    let sourceAccount = getOrCreateAccountBalance(
+      fromAddress,
+      entity as OToken
+    );
+    sourceAccount.balance = sourceAccount.balance.minus(amount);
+    sourceAccount.save();
 
-    let destinationAccount = getOrCreateAccountBalance(toAddress, entity as OToken)
-    destinationAccount.balance = destinationAccount.balance.plus(amount)
-    destinationAccount.save()
+    let destinationAccount = getOrCreateAccountBalance(
+      toAddress,
+      entity as OToken
+    );
+    destinationAccount.balance = destinationAccount.balance.plus(amount);
+    destinationAccount.save();
 
     // exclude txs with options registry and liquidity pool since already handled by OptionsSold and Options Bought
-    if ( !(excludedAddresses.includes(toAddress.toHex()) || excludedAddresses.includes(fromAddress.toHex())) ) { 
-      let sourceAccountPosition = loadOrCreatePosition(fromAddress, event.address.toHex());
-      sourceAccountPosition.netAmount =  sourceAccountPosition.netAmount.minus( amountLP )
-      sourceAccountPosition.longAmount =  sourceAccountPosition.longAmount.minus( amountLP )
-      
+    if (
+      !(
+        excludedAddresses.includes(toAddress.toHex()) ||
+        excludedAddresses.includes(fromAddress.toHex())
+      )
+    ) {
+      let sourceAccountPosition = loadOrCreatePosition(
+        fromAddress,
+        event.address.toHex()
+      );
+      sourceAccountPosition.netAmount = sourceAccountPosition.netAmount.minus(
+        amountLP
+      );
+      sourceAccountPosition.longAmount = sourceAccountPosition.longAmount.minus(
+        amountLP
+      );
 
-      let destinationAccountPosition = loadOrCreatePosition(toAddress, event.address.toHex());
-      destinationAccountPosition.netAmount =  destinationAccountPosition.netAmount.plus( amountLP )
-      destinationAccountPosition.longAmount =  destinationAccountPosition.longAmount.plus( amountLP )
-      
+      let destinationAccountPosition = loadOrCreatePosition(
+        toAddress,
+        event.address.toHex()
+      );
+      destinationAccountPosition.netAmount = destinationAccountPosition.netAmount.plus(
+        amountLP
+      );
+      destinationAccountPosition.longAmount = destinationAccountPosition.longAmount.plus(
+        amountLP
+      );
 
-      const optionsTransferTransaction = new OptionsTransferAction( event.transaction.hash.toHex() )
+      const optionsTransferTransaction = new OptionsTransferAction(
+        event.transaction.hash.toHex()
+      );
 
-      optionsTransferTransaction.otoken = event.address.toHex()
-      optionsTransferTransaction.from = fromAddress
-      optionsTransferTransaction.to = toAddress
-      optionsTransferTransaction.amount = amount
-      optionsTransferTransaction.timestamp = event.block.timestamp
-      optionsTransferTransaction.transactionHash = event.transaction.hash.toHex()
+      optionsTransferTransaction.otoken = event.address.toHex();
+      optionsTransferTransaction.from = fromAddress;
+      optionsTransferTransaction.to = toAddress;
+      optionsTransferTransaction.amount = amount;
+      optionsTransferTransaction.timestamp = event.block.timestamp;
+      optionsTransferTransaction.transactionHash = event.transaction.hash.toHex();
 
-      optionsTransferTransaction.save()
+      optionsTransferTransaction.save();
 
-      const sourceAccountOptionsTransferTransactions = sourceAccountPosition.optionsTransferTransactions
-      sourceAccountOptionsTransferTransactions.push( event.transaction.hash.toHex() )
-      sourceAccountPosition.optionsTransferTransactions = sourceAccountOptionsTransferTransactions   
+      const sourceAccountOptionsTransferTransactions =
+        sourceAccountPosition.optionsTransferTransactions;
+      sourceAccountOptionsTransferTransactions.push(
+        event.transaction.hash.toHex()
+      );
+      sourceAccountPosition.optionsTransferTransactions = sourceAccountOptionsTransferTransactions;
 
-      const destinationAccountOptionsTransferTransactions = destinationAccountPosition.optionsTransferTransactions
-      destinationAccountOptionsTransferTransactions.push( event.transaction.hash.toHex() )
-      destinationAccountPosition.optionsTransferTransactions = destinationAccountOptionsTransferTransactions    
+      const destinationAccountOptionsTransferTransactions =
+        destinationAccountPosition.optionsTransferTransactions;
+      destinationAccountOptionsTransferTransactions.push(
+        event.transaction.hash.toHex()
+      );
+      destinationAccountPosition.optionsTransferTransactions = destinationAccountOptionsTransferTransactions;
 
-
-      sourceAccountPosition.save()
-      destinationAccountPosition.save()
-
+      sourceAccountPosition.save();
+      destinationAccountPosition.save();
     }
-
   }
-
 }
 
-function getOrCreateAccountBalance(address: Address, token: OToken): AccountBalance {
-
+function getOrCreateAccountBalance(
+  address: Address,
+  token: OToken
+): AccountBalance {
   // make sure we create the account entity.
-  let accountEntityId = address.toHex()
-  let account = loadOrCreateAccount(accountEntityId)
-  account.save()
+  let accountEntityId = address.toHex();
+  let account = loadOrCreateAccount(accountEntityId);
+  account.save();
 
-  let entityId = address.toHex() + '-' + token.id;
-  let accountBalance = AccountBalance.load(entityId)
-  if (accountBalance != null) return accountBalance as AccountBalance
+  let entityId = address.toHex() + "-" + token.id;
+  let accountBalance = AccountBalance.load(entityId);
+  if (accountBalance != null) return accountBalance as AccountBalance;
 
-  accountBalance = new AccountBalance(entityId)
-  
+  accountBalance = new AccountBalance(entityId);
+
   accountBalance.token = token.id;
-  accountBalance.account = address.toHex()
-  accountBalance.balance = BIGINT_ZERO
+  accountBalance.account = address.toHex();
+  accountBalance.balance = BIGINT_ZERO;
 
-  return accountBalance as AccountBalance
+  return accountBalance as AccountBalance;
 }
--- src/helper.ts
+++ src/helper.ts
@@ -1,60 +1,62 @@
-import { BigDecimal, BigInt, Bytes, Address } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt, Bytes, Address } from "@graphprotocol/graph-ts";
 
-import {
-  Account,
-  Position
-} from '../generated/schema';
+import { Account, Position } from "../generated/schema";
 
-export let BIGINT_ONE = BigInt.fromI32(1)
-export let BIGINT_ZERO = BigInt.fromI32(0)
-export let BIGDECIMAL_ZERO = BigDecimal.fromString('0')
-export let BIGDECIMAL_ONE = BigDecimal.fromString('1')
+export let BIGINT_ONE = BigInt.fromI32(1);
+export let BIGINT_ZERO = BigInt.fromI32(0);
+export let BIGDECIMAL_ZERO = BigDecimal.fromString("0");
+export let BIGDECIMAL_ONE = BigDecimal.fromString("1");
 
-
-export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
-export const LIQUIDITY_POOL = "0xfd93db0a7c1e373bdfe9b141693a25e4deb79df2"
-export const OPTION_REGISTRY = "0xF37D82c5ee757eE05B08ed469d1CC90c301c8636"
+export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const LIQUIDITY_POOL = "0xfd93db0a7c1e373bdfe9b141693a25e4deb79df2";
+export const OPTION_REGISTRY = "0xF37D82c5ee757eE05B08ed469d1CC90c301c8636";
 
 export function isZeroAddress(value: Address): boolean {
-  return value.toHex() == ZERO_ADDRESS
+  return value.toHex() == ZERO_ADDRESS;
 }
 
-export function updateOptionPosition(isBuy: boolean, trader: Address, oToken: string, amount: BigInt, tradeId: string): void {
+export function updateOptionPosition(
+  isBuy: boolean,
+  trader: Address,
+  oToken: string,
+  amount: BigInt,
+  tradeId: string
+): void {
+  let position = loadOrCreatePosition(trader, oToken);
 
-  let position = loadOrCreatePosition(trader, oToken)
+  position.netAmount = isBuy
+    ? position.netAmount.plus(amount)
+    : position.netAmount.minus(amount);
+  // set position to inactive (closed) whenever we get back to amount = 0
+  if (position.netAmount.isZero()) position.active = false;
 
-  position.netAmount = isBuy ? position.netAmount.plus(amount) : position.netAmount.minus(amount);
-  // set position to inactive (closed) whenever we get back to amount = 0 
-  if (position.netAmount.isZero()) position.active = false
-
-  // let writeOptionsTransactions = position.writeOptionsTransactions 
+  // let writeOptionsTransactions = position.writeOptionsTransactions
   // writeOptionsTransactions.push(tradeId)
   // position.writeOptionsTransactions = writeOptionsTransactions
 
   if (isBuy) {
-    position.longAmount = position.longAmount.plus(amount)
-    const optionsBoughtTransactions = position.optionsBoughtTransactions
-    optionsBoughtTransactions.push(tradeId)
-    position.optionsBoughtTransactions = optionsBoughtTransactions    
+    position.longAmount = position.longAmount.plus(amount);
+    const optionsBoughtTransactions = position.optionsBoughtTransactions;
+    optionsBoughtTransactions.push(tradeId);
+    position.optionsBoughtTransactions = optionsBoughtTransactions;
   } else if (!isBuy) {
-      position.shortAmount = position.shortAmount.minus(amount)
-      const optionsSoldTransactions = position.optionsSoldTransactions
-      optionsSoldTransactions.push(tradeId)
-      position.optionsSoldTransactions = optionsSoldTransactions     
+    position.shortAmount = position.shortAmount.minus(amount);
+    const optionsSoldTransactions = position.optionsSoldTransactions;
+    optionsSoldTransactions.push(tradeId);
+    position.optionsSoldTransactions = optionsSoldTransactions;
   }
 
-  position.save()
+  position.save();
 }
 
-
 export function loadOrCreatePosition(user: Address, oToken: string): Position {
-  let id = user.toHex() + '-' + oToken
+  let id = user.toHex() + "-" + oToken;
   let position = Position.load(id);
   if (position == null) {
     position = new Position(id);
     // make sure there's an account enitity
-    let account = loadOrCreateAccount(user.toHex())
-    account.save()
+    let account = loadOrCreateAccount(user.toHex());
+    account.save();
     position.account = user.toHex();
 
     position.oToken = oToken;
@@ -67,7 +69,6 @@ export function loadOrCreatePosition(user: Address, oToken: string): Position {
     position.optionsSoldTransactions = [];
     position.optionsTransferTransactions = [];
     // position.settleActions = [];
-
   }
   return position as Position;
 }
