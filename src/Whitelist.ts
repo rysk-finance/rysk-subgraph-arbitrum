8d0cd8a234e791761a537a8394777f67bdc0c917
Yassine <yassinesania@gmail.com>
2023-05-14 18:30:24 +0200

    feat(whitelist): Restore checkERC20Entity

--- src/Whitelist.ts
+++ src/Whitelist.ts
@@ -71,12 +71,13 @@ function getProductEntity(
     strike.toString(),
     collateral.toString()
   ]);
-  // let underlyingCheckSuccess = checkERC20Entity(underlying)
-  // let strikeCheckSuccess = checkERC20Entity(strike)
-  // let collateralCheckSuccess = checkERC20Entity(collateral)
+  let underlyingCheckSuccess = checkERC20Entity(underlying);
+  let strikeCheckSuccess = checkERC20Entity(strike);
+  let collateralCheckSuccess = checkERC20Entity(collateral);
 
   // the asset is wrong, (probably not an ERC20), don't create an entity for it.
-  // if (!underlyingCheckSuccess || !strikeCheckSuccess || !collateralCheckSuccess) return null
+  if (!underlyingCheckSuccess || !strikeCheckSuccess || !collateralCheckSuccess)
+    return null;
 
   let id = getProductId(underlying, strike, collateral, isPut);
   let entity = WhitelistedProduct.load(id);
@@ -114,30 +115,32 @@ function getProductId(
  * Make sure the ERC20 entity exist
  * @param address
  */
-// export function checkERC20Entity(address: Address): boolean {
-//   let entity = ERC20.load(address.toHex());
-//   if (entity != null) return true;
-//
-//   log.info("checkERC20Entity createERC20 Entity {} start", [
-//     address.toString()
-//   ]);
-//
-//   entity = new ERC20(address.toHex());
-//   let contract = ERC20Contract.bind(address);
-//
-//   let symbolResult = contract.try_symbol();
-//   if (symbolResult.reverted) return false;
-//   entity.symbol = symbolResult.value as string;
-//
-//   let nameResult = contract.try_name();
-//   if (nameResult.reverted) return false;
-//   entity.name = nameResult.value as string;
-//
-//   let decimalsResult = contract.try_decimals();
-//   if (decimalsResult.reverted) return false;
-//   entity.decimals = decimalsResult.value as i32;
-//   entity.save();
-//
-//   log.info("checkERC20Entity createERC20 Entity {} done", [address.toString()]);
-//   return true;
-// }
+export function checkERC20Entity(address: Address): boolean {
+  let entity = ERC20.load(address.toHex());
+  if (entity != null) return true;
+
+  log.info("checkERC20Entity createERC20 Entity {} start", [
+    address.toString()
+  ]);
+
+  entity = new ERC20(address.toHex());
+
+  // NOTE avoiding contract calls
+  // let contract = ERC20Contract.bind(address);
+
+  // let symbolResult = contract.try_symbol();
+  // if (symbolResult.reverted) return false;
+  entity.symbol = ""; // symbolResult.value as string;
+
+  // let nameResult = contract.try_name();
+  // if (nameResult.reverted) return false;
+  entity.name = ""; // nameResult.value as string;
+
+  // let decimalsResult = contract.try_decimals();
+  // if (decimalsResult.reverted) return false;
+  entity.decimals = 0; // decimalsResult.value as i32;
+  entity.save();
+
+  log.info("checkERC20Entity createERC20 Entity {} done", [address.toString()]);
+  return true;
+}
