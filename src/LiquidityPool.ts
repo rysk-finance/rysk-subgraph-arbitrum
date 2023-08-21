1b2663892cd00550f49197c2e0b23c5bc7ee663a
ugolino <dan.ugolini@gmail.com>
2023-08-21 16:30:20 +0200

    clean code

--- src/LiquidityPool.ts
+++ src/LiquidityPool.ts
@@ -157,19 +157,6 @@ export function handleBuybackOption(event: BuybackOption): void {
   buybackOptionAction.save();
 }
 
-// export function handleDepositEpochExecuted(event: DepositEpochExecuted): void {
-//   const lpContract = liquidityPool.bind(Address.fromString(LIQUIDITY_POOL));
-
-//   const timestamp = event.block.timestamp;
-//   const totalAssets = lpContract.getAssets();
-//   const epoch = event.params.epoch;
-
-//   // const dailyStatSnapshot = getDailySnapshot(timestamp)
-//   // dailyStatSnapshot.totalAssets = dailyStatSnapshot.totalReturns.plus(totalAssets)
-//   // dailyStatSnapshot.epoch = epoch
-//   // dailyStatSnapshot.save()
-// }
-
 export function handleWithdrawalEpochExecuted(
   event: WithdrawalEpochExecuted
 ): void {
