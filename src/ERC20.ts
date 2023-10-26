d733bee1baecb27effd5a1217eb0b19f7dac0de4
Tim <timothy.dunphy@gmail.com>
2023-10-26 15:52:59 +0100

    Refactor logic to be mapped by address.

--- src/ERC20.ts
+++ src/ERC20.ts
@@ -2,28 +2,69 @@ import { Address } from '@graphprotocol/graph-ts'
 
 import { Transfer } from '../generated/ARB/ERC20'
 import { chainlinkAggregator } from '../generated/ARB/chainlinkAggregator'
-import { ARBTreasuryAirdrop } from '../generated/schema'
-import { CHAINLINK_AGGREGATOR_ARB_USD, TREASURY } from './helper'
+import { AirdropRecipient, AirdropTransaction } from '../generated/schema'
+import { CHAINLINK_AGGREGATOR_ARB_USD, TREASURY } from './addresses'
+import { BIG_DECIMAL_1e18, BIG_DECIMAL_1e8 } from './constants'
 
 export function handleTransfer(event: Transfer): void {
+  const amount = event.params.value
+  const fromAddress = event.params.from
+  const toAddress = event.params.to
+  const transactionHash = event.transaction.hash.toHex()
+
+  // get current ARB price.
   const chainlinkAggregatorContractARB = chainlinkAggregator.bind(Address.fromString(CHAINLINK_AGGREGATOR_ARB_USD))
   const arbPrice = chainlinkAggregatorContractARB.latestAnswer()
 
-  const fromAddress = event.params.from
+  // Get USD value.
+  const price = arbPrice.toBigDecimal().div(BIG_DECIMAL_1e8)
+  const value = amount
+    .toBigDecimal()
+    .div(BIG_DECIMAL_1e18)
+    .times(price)
 
   if (fromAddress.toHexString() == TREASURY) {
-    const entity = new ARBTreasuryAirdrop(event.transaction.hash.toHex())
+    // Create transaction entity.
+    const newTransaction = new AirdropTransaction(transactionHash)
 
-    entity.address = event.address
-    entity.decimals = 18
-    entity.name = 'Arbitrum'
-    entity.symbol = 'ARB'
+    newTransaction.address = event.address
+    newTransaction.decimals = 18
+    newTransaction.name = 'Arbitrum'
+    newTransaction.symbol = 'ARB'
 
-    entity.amount = event.params.value
-    entity.arbPrice = arbPrice
-    entity.from = fromAddress
-    entity.to = event.params.to
+    newTransaction.amount = amount
+    newTransaction.arbPrice = arbPrice
+    newTransaction.from = fromAddress
+    newTransaction.to = toAddress
 
-    entity.save()
+    newTransaction.save()
+
+    // Load existing user and update with new transaction.
+    const user = AirdropRecipient.load(toAddress.toHex())
+
+    if (user) {
+      const userTransactions = user.transactions
+      userTransactions.push(transactionHash)
+      user.transactions = userTransactions
+
+      const totalTokens = user.totalTokens
+      user.totalTokens = totalTokens.plus(amount)
+
+      const totalValue = user.totalValue
+      user.totalValue = totalValue.plus(value)
+
+      user.save()
+
+      return
+    }
+
+    // If no user was found, create a new one.
+    const newUser = new AirdropRecipient(toAddress.toHex())
+
+    newUser.totalTokens = amount
+    newUser.totalValue = value
+    newUser.transactions = [transactionHash]
+
+    newUser.save()
   }
 }
--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,9 @@
-import { BigInt } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 
 export const BIGINT_ONE = BigInt.fromI32(1)
 export const BIGINT_ZERO = BigInt.fromI32(0)
+export const BIG_DECIMAL_1e8 = BigDecimal.fromString('100000000')
+export const BIG_DECIMAL_1e18 = BigDecimal.fromString('1000000000000000000')
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
