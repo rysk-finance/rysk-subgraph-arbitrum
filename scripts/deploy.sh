dfe834f77776b82d04c270cee78df43cee06924c
Tim <timothy.dunphy@gmail.com>
2023-11-01 13:20:28 +0000

    Now deploying to TheGraph on mainnet.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,18 @@ if [[ -f $env ]]; then
   set +a
 fi
 
-GOLDSKY_TOKEN=$(printenv GOLDSKY_TOKEN)
-
 deployment_name=$1
 deployment_version=$(cat package.json | jq .version -r)
 deployment=$deployment_name/$deployment_version
 flags=${@:2}
+
+# Deploy TheGraph only on mainnet.
+if [[ $deployment_name == arbitrum-one ]]; then
+  graph \
+    deploy rysk-finance/$deployment_name \
+    --product hosted-service \
+    --deploy-key $THE_GRAPH_TOKEN
+fi
 
 goldsky \
   subgraph deploy $deployment \
